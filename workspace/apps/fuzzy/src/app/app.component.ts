f3c820ff5e891d6f981a2b317c9456aba63063da
Matt Vaughn <matt.vaughn@buildmotion.com>
2021-05-16 10:12:07 -0600

    fuzzy eslint initial checkin

---
+++ workspace/apps/fuzzy/src/app/app.component.ts
@@ -0,0 +1,17 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'workspace-root',
+  templateUrl: './app.component.html',
+  styleUrls: ['./app.component.scss'],
+})
+export class AppComponent implements OnInit {
+  title = 'fuzzy';
+
+  constructor() {
+    console.log('Happy component here.');
+  }
+  ngOnInit(): void {
+    // throw new Error('Method not implemented.');
+  }
+}
